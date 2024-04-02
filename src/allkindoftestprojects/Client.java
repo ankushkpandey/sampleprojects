7b315ac85e43500a7ab3ef3569550a0ff817b27b
ankush kumar <ankushkumar78900@gmail.com>
2024-04-02 16:02:43 +0530

    commit from office

---
+++ src/allkindoftestprojects/Client.java
@@ -0,0 +1,7 @@
+package allkindoftestprojects;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Test Purpose Only");
+    }
+}
---
+++ src/allkindoftestprojects/Test.java
@@ -0,0 +1,14 @@
+package allkindoftestprojects;
+
+public class Test {
+    private int i =0;
+
+    public Test(int i) {
+        this.i = i;
+    }
+
+    public int testProject(){
+        System.out.println("Test is being Done");
+        return 0;
+    }
+}
